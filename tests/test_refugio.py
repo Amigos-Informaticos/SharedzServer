da04ec3a6e39d00f46445608de28e0f999baa749
w3edd <edsonmanuelcarballovera@gmail.com>
2021-10-13 16:34:17 -0500

    Se han creado las pruebas para el refugio

---
+++ tests/test_refugio.py
@@ -0,0 +1,19 @@
+from src.routes.HTTPStatus import OK, RESOURCE_CREATED
+
+refugio = Refugio()
+
+
+def test_guardar():
+	assert refugio.guardar() == RESOURCE_CREATED
+
+
+def test_actualizar():
+	assert refugio.actualizar() == OK
+
+
+def test_cargar():
+	assert refugio.cargar() == True
+
+
+def test_eliminar():
+	assert refugio.eliminar() == OK
