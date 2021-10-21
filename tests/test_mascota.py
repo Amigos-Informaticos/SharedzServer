85fc0bf3105b2f5e953d2e5487aaee35c55201f0
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-10-20 23:06:43 -0500

    0.4.0 Creacion de modelo de Mascota y pruebas unitarias (#15)
    * Se han corregido problemas en el modelo de Adoptante
    
    * Se ha corregido el timepo de vida del token en la configuracion inicial del sistema
    
    * Se ha corregido la manipulación de fechas en la clase Adoptante
    
    * Se ha añadido cifrado SSL a la conexión
    
    * Se han actualizado los certificados SSL
    
    * Se ha cambiado el comportamiento del metodo eliminar del adoptante
    
    * Se han creado las pruebas para el refugio
    
    * Se han cambiado los certificados SSL
    
    * Se han omitido temporalmente las pruebas de refugio
    
    * Se han creado los metodos del modelo Refugio
    
    * Se han corregido problemas menores y las pruebas se ejecutan correctamente
    
    * Correccion recomendada por Codacy
    
    * Se ha reducido la complejidad del codigo de Auth
    
    * Se ha actualizado la especificacion de la API
    
    * Se han separado los flujos del workflow
    
    * Se han dividido los flujos
    
    * Se ha corregido las condiciones de ejecucion del flujo de construccion de la imagen de Docker
    
    * Se ha añadido una condicional para la ejecución del workflow de construccion de la imagen
    
    * Se ha eliminado una condicional de ejecucion de workflow
    
    * Se ha actualizado la URL del servidor de producción en la especificación de la API
    
    * Se ha corregido un pequeño error en el archivo del workflow de construcción
    
    * Se ha corregido el archivo del workflow para construir la imagen
    
    * Se ha modificado la condicional para ejecutar el workflow de construir la imagen
    
    * Se ha actualizado el componente Persona en la especificacion de la API
    
    * Se ha creado un paso para lanzar un fallo en caso de que no se cumpla la condicion en el flujo de construccion
    
    * Se ha actualizado la versión de la especificación
    
    * Se han actualizado los payloads de varios endpoints para reflejar la estructura del payload y no del componente
    
    * Se han creado los endpoints principales de refugio
    
    * Se ha cambiado una condicional en el workflow para construir una imagen
    
    * Se han reunido los workflows bajo un mismo archivo
    
    * Se han divido los workflows a diferentes disparadores
    
    * Se han añadido las rutas de refugio al lanzador de flask
    
    * Se ha corregido el flujo de construcción de la imagen de Docker
    
    * Se ha eliminado la construcción de la imagen en PR
    
    * Se ha corregido un error en la respuesta del endpoint para registrar un refugio
    
    * Se ha corregido un error en la respuesta del endpoint para registrar un refugio
    
    * Se han realizado correcciones al endpoint para actualizar el refugio
    
    * Se ha actualizado la especificacion de la API
    
    * Se ha creado el modelo de Mascota y las pruebas unitarias correspondientes

---
+++ tests/test_mascota.py
@@ -0,0 +1,32 @@
+import pytest
+
+from src.model.Mascota import Mascota
+from src.routes.HTTPStatus import OK, RESOURCE_CREATED
+
+mascota = Mascota()
+mascota.nombre = "Mauricio"
+
+
+@pytest.mark.dependency()
+def test_guardar():
+	estado = mascota.guardar()
+	assert estado == RESOURCE_CREATED
+
+
+@pytest.mark.dependency(depends=["test_guardar"])
+def test_actualizar():
+	assert mascota.id_mascota is not None
+	mascota.peso = 2
+	mascota.color = "Rojecito"
+	assert mascota.actualizar() == OK
+
+
+@pytest.mark.dependency(depends=["test_guardar"])
+def test_cargar():
+	assert mascota.cargar() == True
+	print(mascota.__dict__)
+
+
+@pytest.mark.dependency(depends=["test_guardar"])
+def test_eliminar():
+	assert mascota.eliminar() == OK
