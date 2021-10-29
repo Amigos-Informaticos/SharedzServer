bbec4a26fcc4a8bc3e206643f203328936c1625d
Edson Manuel Carballo Vera <edsonmanuelcarballovera@gmail.com>
2021-10-28 21:39:34 -0500

    Update test_adoptante.py

--- tests/test_adoptante.py
+++ tests/test_adoptante.py
@@ -10,7 +10,7 @@ adoptante.nombre = "Miguel Joaquin"
 def test_guardar():
 	assert adoptante.guardar() == RESOURCE_CREATED
 
-  
+
 def test_actualizar():
 	assert adoptante.id_adoptante is not None
 	adoptante.cargar_adoptante()
